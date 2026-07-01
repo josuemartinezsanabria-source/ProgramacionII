51c1e88dc0626f2157ae536c6b64641a08c4b37a
Reshi1305J <josuemartinezsanabria@gmail.com>
2026-06-30 20:29:20 -0600

    feat: se agrega funcion maximo

--- Practica.py
+++ Practica.py
@@ -7,4 +7,8 @@ print("Dimensión de la tabla:", df.shape)
 
 # Función que retorna el menor de tres valores
 def menor(A, B, C):
+    return min(A, B, C) 
+
+def maximo(A, B, C):
     return min(A, B, C)
+
